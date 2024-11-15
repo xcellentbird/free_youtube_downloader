832fad52540b1eebda4715a72e4a3ba9535c30b9
mike <xcellentbird@gmail.com>
2024-11-16 01:14:02 +0900

    테스트 main 코드

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from pytube import YouTube
 
+context = {"Host":"freeyoutubedownloader-dflb5w4bmiyxxac4mespbr.streamlit.app","User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36","Accept-Encoding":"gzip, deflate, br, zstd","Accept-Language":"ko,ko-KR;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6","Cache-Control":"no-cache","Connection":"Upgrade","Origin":"https://freeyoutubedownloader-dflb5w4bmiyxxac4mespbr.streamlit.app","Pragma":"no-cache","Sec-Websocket-Extensions":"permessage-deflate; client_max_window_bits","Sec-Websocket-Key":"i5/fqgDHCW0fLLtF1FPZ5Q==","Sec-Websocket-Protocol":"streamlit, PLACEHOLDER_AUTH_TOKEN","Sec-Websocket-Version":"13","Upgrade":"websocket","X-Forwarded-For":"192.168.0.69, 10.12.39.115","X-Streamlit-User":"eyJlbWFpbCI6IiIsImlzUHVibGljQ2xvdWRBcHAiOnRydWV9"}
 
 def form_url_input():
     with st.form(key="url_input"):
@@ -10,8 +11,8 @@ def form_url_input():
             if url:
                 yt = YouTube(
                     url=url,
+                    proxies={"https": "https://freeyoutubedownloader-dflb5w4bmiyxxac4mespbr.streamlit.app"},
                 )
-                st.write(st.context.headers.to_dict())
                 video = yt.streams.filter(progressive=True, file_extension="mp4").first()
 
                 st.video(video.url)
