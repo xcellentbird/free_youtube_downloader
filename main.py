37ec88e662079854711759aa1b7c34b58113e54d
mike <xcellentbird@gmail.com>
2024-11-16 01:09:17 +0900

    테스트 main 코드

---
+++ main.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from pytube import YouTube
+
+
+def form_url_input():
+    with st.form(key="url_input"):
+        url = st.text_input("Youtube URL", value="https://www.youtube.com/watch?v=u81OeTGT7WA&t=454s")
+
+        if st.form_submit_button("Submit"):
+            if url:
+                yt = YouTube(
+                    url=url,
+                )
+                video = yt.streams.filter(progressive=True, file_extension="mp4").first()
+
+                st.video(video.url)
+
+    return url
+
+
+def main():
+    st.title("Hello, World!")
+    form_url_input()
+
+
+if __name__ == "__main__":
+    main()
